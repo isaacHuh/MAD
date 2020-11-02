316e7c01bf1fe964cdd72777ccedcd04f304e9a1
unknown <torresisaac26@gmail.com>
2020-11-02 12:02:52 -0800

    demo scene

---
+++ MAD_Project/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+
+    public float xRotation = 0f;
+
+    float angRot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.LeftControl))
+        {
+            return;
+        }
+
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        angRot += Time.deltaTime;
+
+        angRot %= (2 * Mathf.PI);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, Mathf.Sin(angRot)); // Mathf.Sin(angRot)
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ MAD_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    float pushPower = 2.0f;
+    float weight = 6.0f;
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+        Vector3 force;
+
+        // no rigidbody
+        if (body == null || body.isKinematic) { return; }
+
+        // We use gravity and weight to push things down, we use
+        // our velocity and push power to push things other directions
+        if (hit.moveDirection.y < -0.3)
+        {
+            force = new Vector3(0, -0.5f, 0) * gravity * weight;
+        }
+        else
+        {
+            force = hit.controller.velocity * pushPower;
+        }
+
+        // Apply the push
+        body.AddForceAtPosition(force, hit.point);
+    }
+}
