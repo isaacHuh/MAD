ee7d5d25811b6f5c0d2127a667c387088978d1a8
Foreverowlone <60590212+Foreverowlone@users.noreply.github.com>
2020-12-02 21:00:13 -0800

    Timer, scenes, buttons, scripts.

---
+++ MAD_Project/Assets/Christiana/Scripts/GameOverScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour
+{
+    public TimerSO timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer.value <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        //Scene change on death (timer <= 0)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("DeathScreen");
+        Debug.Log("GAME OVER");
+    }
+}
---
+++ MAD_Project/Assets/Christiana/Scripts/TimerSO.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Timer", menuName = "Assets/Timer")]
+public class TimerSO : ScriptableObject
+{
+    public float value;
+
+    public bool reset;
+
+    public float resetValue;
+
+    public void resetTimer()
+    {
+        if (reset)
+        {
+            value = resetValue;
+            reset = false;
+        }
+    }
+
+    public void updateTimer(float timeToRemove)
+    {
+
+        value -= timeToRemove;
+    }
+
+    public void setReset()
+    {
+        reset = true;
+    }
+}
