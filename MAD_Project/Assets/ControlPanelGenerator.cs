d1c40b49edd4bdd52ef739d0269ab2bd272e5449
unknown <torresisaac26@gmail.com>
2020-12-06 18:54:58 -0800

    added control panel generation, task scripts for tracking and shader effects

---
+++ MAD_Project/Assets/ControlPanelGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanelGenerator : MonoBehaviour
+{
+    public List<GameObject> tasks;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int taskInt = Random.Range(0,tasks.Count);
+        for(int i = 0; i < 4; i++){
+            int newTaskInt = Random.Range(0,tasks.Count);
+            if(newTaskInt == taskInt){
+                i--;
+                continue;
+            }else{
+                taskInt = newTaskInt;
+            }
+            Instantiate(tasks[taskInt],transform.position + new Vector3(0,0.25f*i,0),transform.rotation,transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
